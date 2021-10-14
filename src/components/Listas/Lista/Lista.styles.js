b8d0ba6251ee17ee61c6ae869dc371a1ef3d31c0
201801652@est.umss.edu <lluscomirandaneida10@gmail.com>
2021-10-14 00:36:14 -0400

    Lista de cursos implementada

---
+++ src/components/Listas/Lista/Lista.styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+export const Recuadro = styled.div`
+
+
+
+`;
+
+export const ImagenL = styled.img`
+
+
+
+`;
+export const NombreIL = styled.h1`
+
+
+
+`;
+export const AutorL = styled.p`
+
+
+
+`;
+export const CantInsL = styled.h1`
+
+
+
+`;
+
