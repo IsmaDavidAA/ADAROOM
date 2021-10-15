92d580058182408e9689e2cfd887ce101552d9f9
pedromq <201801941@est.umss.edu>
2021-10-15 17:02:58 -0400

    Se agrego el menu-bar

---
+++ src/components/Menu/Menu.styles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+export const MenuEstilo = styled.div`
+    background-color: #f8f8f8f8;
+    padding: 0.09rem 0;
+    box-shadow: 0px 0px 10px 0 #999999;
+    position: fixed;
+    z-index: 10;
+    left: 0;
+    right: 0;
+    top: 0;
+    
+    
+    
+`;
+
+export const ImagenEstilo = styled.img`
+    width: 258px; 
+    height: 88px;
+    margin-left: 50px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+`;
+
+export const InicioEstilo = styled.a`
+    color: #1E3163;
+    font-weight: bold;
+    font-size: 20px ;
+    font-family: 'Segoe UI';
+    position: relative;
+    bottom: 50px;
+    padding-left: 150px;
+    text-decoration: none;
+`;
+
+export const CursoEstilo = styled.a`
+    font-size: 20px ;
+    font-weight: bold;
+    color: #1E3163;
+    font-family: 'Segoe UI';
+    position: relative;
+    bottom: 50px;
+    padding-left: 50px;
+    text-decoration: none; 
+`;
