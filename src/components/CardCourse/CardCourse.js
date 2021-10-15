54dd1c63e1deeaa6edc6fcb629104fe3ee0690db
Carolina-Melgarejo <cmelgarejo38@gmail.com>
2021-10-15 16:59:09 -0400

    componente CardCourse agregado

---
+++ src/components/CardCourse/CardCourse.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Imagen from '../../images/java.jpg'
+import { StyledCard, CardContainer, ImageC, Course, Institution, Inscribed } from './StyleList'
+
+export default function CardCourse({ codigo, nombreCurso, institucion, imagen, cantInscritos }) {
+
+    return (
+        <StyledCard className='d-flex'>
+            <ImageC className='p-15px' src={Imagen} alt="" srcset="" width="300" />
+            <CardContainer className='p-15px'>
+                <Course className='text-bold space'>{nombreCurso}</Course>
+                <Institution className='text-gray space'>{institucion}</Institution>
+                <Inscribed className='text-bold'>{cantInscritos} Participantes</Inscribed>
+            </CardContainer>
+        </StyledCard>
+    )
+}
