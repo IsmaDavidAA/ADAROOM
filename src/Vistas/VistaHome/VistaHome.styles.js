c43b4e9ec29e81e4857e78e959b2b26fac542cfa
Ludwing Cruz Flores <ludwing22cruz@gmail.com>
2021-10-23 00:47:57 -0400

    mosaicos y metodos de los cursos

--- src/Vistas/VistaHome/VistaHome.styles.js
+++ src/Vistas/VistaHome/VistaHome.styles.js
@@ -4,3 +4,18 @@ export const Contenedor = styled.div`
   min-height: 80vh;
   width: 100%;
 `;
+export const Coment = styled.div`
+  font-weight: bold;
+  font-size: var(--letraEstandar);
+  margin: 4rem 0 0 9rem;
+`;
+export const Coment1 = styled.div`
+  font-size: var(--letraEstandar);
+  margin: 1rem 0 0 9rem;
+`;
+export const MasCursos = styled.div`
+  color: #0061a8;
+  font-size: var(--letraEstandar);
+  margin: 1.5rem 0 13rem 9rem;
+  font-weight: bold;
+`;
