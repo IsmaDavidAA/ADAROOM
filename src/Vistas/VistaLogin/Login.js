f2aee3d7ebc715ffb0d48c9f06a64d3fccc101d3
kiberMena <73322760+kiberMena@users.noreply.github.com>
2021-11-08 14:11:57 -0400

    afregar el componente de login

--- src/Vistas/VistaLogin/Login.js
+++ src/Vistas/VistaLogin/Login.js
@@ -1,25 +1,28 @@
 import React, {useCallback, useContext} from 'react';
-import {Contenedor} from "./Login.styles"
 //import { FirebaseAuth } from "react-firebaseui";
 import  {auth} from "../../services/firebase"
 import { AuthContext } from "../../Context";
 import { withRouter, Redirect } from "react-router-dom";
 import {signInWithEmailAndPassword} from "@firebase/auth";
-
+import Index from '../../components/SignIn/Index';
 
 const SignIn = ({history}) => {
   //Obtenemos el estado del user en el context
-  const handleLogin = useCallback(
+   const handleLogin = useCallback(
     async event => {
       event.preventDefault();
       const { email, password } = event.target.elements;
+      console.log(email.value)
       try {
         await 
           signInWithEmailAndPassword(auth,email.value, password.value);
         history.push("/");
+        console.log(AuthContext);
+        
       } catch (error) {
         alert(error);
       }
+      console.log("asdasasdasdasdadasdasdasdasdasdasdasdasd")
     },
     [history]
   );
@@ -28,21 +31,8 @@ const SignIn = ({history}) => {
        return <Redirect to ="/" />;
      }
     return (
-      <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
-      
-    </div>
+      <Index handleLogin={handleLogin}/>
+     
 
   );
 };
