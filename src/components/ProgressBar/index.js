481768a35aa52ea94098655976b2d262cd5bc917
Adrian <hilariadrian@gmail.com>
2021-11-25 20:23:26 -0400

    Agregando estilos a la barra estatica del ProgressBar

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 const progressBar = ({progress}) => {
-		
+
+    const Parentdiv = {
+		height: '30px',
+		width: '420px',
+		backgroundColor: 'var(--plomoAgua)',
+		borderRadius: 5,
+	}
+
 	return (
 	<div >
-		<div >
+		<div style={Parentdiv}>
 			<span >{`${progress}%`}</span>
 		</div>
 	</div>
