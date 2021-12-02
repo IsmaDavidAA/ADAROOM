f0999a063d52e46ec05ae3a16d624c4945714a08
LudwingCruz9320 <92557666+LudwingCruz9320@users.noreply.github.com>
2021-12-02 12:41:58 -0400

    header desde 0 para que pueda ser responsive

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -4,6 +4,7 @@ import imgusuario from "../../images/user.png";
 import { getAuth, signOut } from "firebase/auth";
 import { useHistory } from "react-router-dom";
 
+import { FaBars, FaTimes } from "react-icons/fa";
 
 import {
   MenuEstilo,
@@ -19,7 +20,13 @@ import {
   Mensage,
   Contenedor,
   Estilobarra,
-  nombrebutton
+  nombrebutton,
+  Container,
+  Wrapper,
+  Menun,
+  MenuItem,
+  MenuItemLink,
+  MobileIcon,
 } from "./Menu.styles";
 import {
   BrowserRouter as Router,
@@ -32,11 +39,13 @@ import {
 import { AuthContext } from "../../Context";
 import { apiSettings } from "../../services/services";
 
+import { IconContext } from "react-icons";
+
 function Menu() {
   const [user, setUser] = useState([]);
   const [nombre, setNombre] = useState("");
   const { currentUser } = useContext(AuthContext);
-  
+
   const fetchName = async () => {
     if (currentUser) {
       const temp = await apiSettings.getName(currentUser.uid);
@@ -49,10 +58,10 @@ function Menu() {
     fetchName();
   }, [currentUser]);
 
-
   const auth = getAuth();
-  const history = useHistory();
-  
+
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
   if (currentUser) {
     return (
       <MenuEstilo>
@@ -68,48 +77,75 @@ function Menu() {
         <Link to={`/misCursos`} id="este">
           <MisCuros> MIS CURSOS</MisCuros>
         </Link>
-          <Estudiate> {nombre} </Estudiate>        
-        <TrianguloEstilo href = "#"> &#x25BC; </TrianguloEstilo>
-       <Estilobarra>
-        <Link to= {`/`}>
-        <button
-          onClick={() => {
-            signOut(auth)
-              .then(() => {
-                window.location.reload();
-               // history.push("/");
-              })
-              .catch((error) => {
-                // An error happened.
-              });
-          }}
-        >
-          {""}
-
-          Cerrar Sesión
-        </button>
-        </Link>
+        <Estudiate> {nombre} </Estudiate>
+        <TrianguloEstilo href="#"> &#x25BC; </TrianguloEstilo>
+        <Estilobarra>
+          <Link to={`/`}>
+            <button
+              onClick={() => {
+                signOut(auth)
+                  .then(() => {
+                    window.location.reload();
+                    // history.push("/");
+                  })
+                  .catch((error) => {
+                    // An error happened.
+                  });
+              }}
+            >
+              {""}
+              Cerrar Sesión
+            </button>
+          </Link>
         </Estilobarra>
-       <UsuarioImagen src={imgusuario} />
+        <UsuarioImagen src={imgusuario} />
       </MenuEstilo>
-
     );
   } else {
     return (
-      <MenuEstilo>
-        <Link to={`/`}>
-          <ImagenEstilo src={ada} />
-        </Link>
-        <Link to={`/`}>
-          <InicioEstilo> INICIO</InicioEstilo>
-        </Link>
-        <Link to={`/cursos`}>
-          <CursoEstilo> CURSOS</CursoEstilo>
-        </Link>
-        <Link to={`/login`}>
-          <IniciaSecion> INICIAR SESIÓN</IniciaSecion>
-        </Link>
-      </MenuEstilo>
+      <Container>
+        <Wrapper>
+          <IconContext.Provider value={{ style: { fontSize: "2em" } }}>
+            <Link to={`/`}>
+              <ImagenEstilo src={ada} />
+            </Link>
+
+            <MobileIcon onClick={() => setShowMobileMenu(!showMobileMenu)}>
+              {showMobileMenu ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+
+            <Menun open={showMobileMenu}>
+              <MenuItem>
+                <MenuItemLink
+                  onClick={() => setShowMobileMenu(!showMobileMenu)}
+                >
+                  <Link to={`/`}>
+                    <InicioEstilo> INICIO</InicioEstilo>
+                  </Link>
+                </MenuItemLink>
+              </MenuItem>
+              <MenuItem>
+                <MenuItemLink
+                  onClick={() => setShowMobileMenu(!showMobileMenu)}
+                >
+                  <Link to={`/cursos`}>
+                    <CursoEstilo> CURSOS</CursoEstilo>
+                  </Link>
+                </MenuItemLink>
+              </MenuItem>
+              <MenuItem>
+                <MenuItemLink
+                  onClick={() => setShowMobileMenu(!showMobileMenu)}
+                >
+                  <Link to={`/login`}>
+                    <IniciaSecion> INICIAR SESIÓN</IniciaSecion>
+                  </Link>
+                </MenuItemLink>
+              </MenuItem>
+            </Menun>
+          </IconContext.Provider>
+        </Wrapper>
+      </Container>
     );
   }
 }
