ee9bb41d1ed50b58fc704d3ba419b24d5867e7f8
kiberMena <73322760+kiberMena@users.noreply.github.com>
2021-12-02 22:57:06 -0400

    115 se implemeto el componente caja

---
+++ src/components/Caja/Caja.styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+export const EstiloCaja = styled.div`
+display:flex; 
+background:none ; 
+ cursor: context-menu;
+
+`;
+export const Pdf = styled.img`
+width:45px; 
+height:45px;
+margin-top: 30px;
+
+`;
+export const Video = styled.img`
+width:45px; 
+height:45px;
+margin-top: 30px;
+
+`;
+
+export const Check = styled.input`
+transform: scale(2);
+color:red;
+
+`;
+export const Titulo = styled.a`
+
+text-decoration:none;
+color:black; 
+font-size: 20px;
+font-weight:bold;
+margin-left:18px;
+margin-top: 30px;
+`;
